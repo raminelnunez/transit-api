d4559f583a74637f152fdcc446f0b666c1d97b1d
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-09 15:38:19 -0600

    defined html elements in js

---
+++ app.js
@@ -0,0 +1,6 @@
+const html = {
+  form: document.getElementsByTagName('form')[0],
+  streets: document.getElementsByClassName('streets')[0],
+  streetName: document.getElementById('stree-name'),
+  resultsContainer: document.getElementsByTagName('tbody')[0],
+}
