e56e241722b4a512807e464a5cdac11c8e61ffbc
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-09 15:48:30 -0600

    defining url

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const html = {
   streetName: document.getElementById('stree-name'),
   resultsContainer: document.getElementsByTagName('tbody')[0],
 }
+
+//https://api.winnipegtransit.com/v3/stops/10064?api-key=1vbBsFqGS6MWA0hvpBq0
+
+const apiKey = '1vbBsFqGS6MWA0hvpBq0';
+const baseURL = 'https://api.winnipegtransit.com/v3/';
